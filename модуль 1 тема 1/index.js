1a7823a531db37c61916fe7a23bec8273f1b714b
Надежда Лыскова <nadya1702@yandex.ru>
2025-01-21 08:35:11 +0300

    Добавлен второй товар в index.js

--- модуль 1 тема 1/index.js
+++ модуль 1 тема 1/index.js
@@ -1,9 +1,21 @@
 'use strict';
 
-const item = 'Компьютерный стол';
-const count = 5;
-const category = 'Офисная мебель';
-const price = 4792;
+{
+    const item = 'Компьютерный стол';
+    const count = 5;
+    const category = 'Офисная мебель';
+    const price = 4792;
 
-console.log(item);
-console.log('Общая сумма товара: ', count * price, 'руб.');
+    console.log(item);
+    console.log('Общая сумма товара: ', count * price, 'руб.');
+}
+
+{
+    const item = 'Тетрадь';
+    const count = 8;
+    const category = 'Канцелярские товары';
+    const price = 79;
+
+    console.log(item);
+    console.log('Общая сумма товара: ', count * price, 'руб.');
+}
