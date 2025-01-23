3d6923cf831c574066c7ec76f53f795e570d7f79
Надежда Лыскова <nadya1702@yandex.ru>
2025-01-23 23:07:40 +0300

    добавлен и подключен файл einstein.js с задачей №2

---
+++ module 1 topic 3/einstein.js
@@ -0,0 +1,10 @@
+'use strict';
+
+{
+    const myWeight = 50;
+    const speedLight = 3e8;
+
+    const energy = myWeight * speedLight**2;
+
+    console.log(`Рассчитанная по формуле Эйнштейна энергия тела массой ${myWeight} кг равна ${energy} Дж.`);
+}
