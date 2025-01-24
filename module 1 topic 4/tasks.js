7cf66686cfdfd0123e981dc5e67a05289de533bf
Надежда Лыскова <nadya1702@yandex.ru>
2025-01-24 23:04:58 +0300

    добавлен и подключен файл tasks.js + добавлено второе задание

---
+++ module 1 topic 4/tasks.js
@@ -0,0 +1,14 @@
+'use strict';
+
+//Вторая задача
+{
+    const rain = Math.round(Math.random());
+
+    if (rain === 1) {
+    console.log('Пошёл дождь. Возьмите зонт!');
+    }
+
+    if (rain === 0) {
+    console.log('Дождя нет!');
+    }
+}
