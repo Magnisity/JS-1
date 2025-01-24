d1eb439e948e4dc4b097fba8822981db34d038de
Надежда Лыскова <nadya1702@yandex.ru>
2025-01-25 01:07:24 +0300

    в index.js доработано задание 1; в tasks.js добавлено задание 3

--- module 1 topic 4/index.js
+++ module 1 topic 4/index.js
@@ -1,14 +1,22 @@
 'use strict';
 
+//Первая задача
 {
     const item = prompt('Введите наименование товара:', 'Компьютерный стол');
-    let count = +prompt('Введите количество товара:', '5');  //правильно ли сразу писать +promt, чтобы преобразовать в число? или лучше это делать где-то на другом шаге?
+    let count = prompt('Введите количество товара:', '5');
     const category = prompt('Введите категорию товара:', 'Офисная мебель');
-    let price = +prompt('Введите цену товара:', '4500');
+    let price = prompt('Введите цену товара:', '4500');
 
-    if (count > 0 && price > 0 && Number.isInteger(count) && Number.isFinite(count) && Number.isFinite(price)) {
-        console.log(`Вы заказали ${count} шт товара "${item}" на общую сумму ${count * price} руб.`);
+    if (count === null || price === null) {
+        console.log('Для подсчета необходимо ввести количество и цену товара.');
     } else {
-        console.log('Вы ввели некорректные данные');
+        count = Number(count);  //почему в этих двух строках перевод в число с помощью "+" (+count, +price) не работает?
+        price = Number(price);
+
+        if (count > 0 && price > 0 && Number.isInteger(count) && Number.isFinite(count) && Number.isFinite(price)) {
+            console.log(`Вы заказали ${count} шт товара "${item}" на общую сумму ${count * price} руб.`);
+        } else {
+            console.log('Вы ввели некорректные данные');
+        }
     }
 }
--- module 1 topic 4/tasks.js
+++ module 1 topic 4/tasks.js
@@ -12,3 +12,29 @@
     console.log('Дождя нет!');
     }
 }
+
+//Третья задача
+{
+    let resultMath = prompt('Введите количество баллов по математике:');
+    let resultRus = prompt('Введите количество баллов по русскому языку:');
+    let resultInform = prompt('Введите количесвто баллов по информатике:');
+
+    if (resultMath === null || resultRus === null || resultInform === null) {
+        console.log('Для правильного подсчета введите полученные баллы по всем трём предметам.');
+    } else {
+        resultMath = Number(resultMath);
+        resultRus = Number(resultRus);
+        resultInform = Number(resultInform);
+        const totalResults = resultMath + resultRus + resultInform;
+
+        if (resultMath >= 0 && resultRus >= 0 && resultInform >= 0 && Number.isFinite(resultMath) && Number.isFinite(resultRus) && Number.isFinite(resultInform)) {
+            if (totalResults >= 265) {
+                console.log(`Ваше количество баллов в сумме равно ${totalResults}. Поздравляем, вы поступили на бюджет!`);
+            } else {
+                    console.log(`Ваше количество баллов в сумме равно ${totalResults}. К сожалению, для поступления на бюджет этого недостаточно.`);
+            }
+        } else {
+        console.log('Вы ввели некорректные данные.');
+        }
+    }
+}
