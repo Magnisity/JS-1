f642a7e9202470229ece319a78e1cffe4f2ee9d1
Надежда Лыскова <nadya1702@yandex.ru>
2025-01-25 16:19:01 +0300

    добавлен и подключен файл tasksHard_2.js с доп.задачей 2; доработано доп.задание 1 в tasksHard.1.js

--- module 1 topic 4/tasksHard_2.js
+++ module 1 topic 4/tasksHard_2.js
@@ -13,10 +13,10 @@ if (income === null) {
         tax = Math.round(income * 0.13);
         console.log(`Сумма налога составляет ${tax} руб.`);
     } else if (income > 15000 && income <= 50000 && Number.isFinite(income)) {
-        tax = Math.round(income * 0.20);
+        tax = Math.round(15000 * 0.13 + (income - 15000)*0.2);
         console.log(`Сумма налога составляет ${tax} руб.`);
     } else if (income > 50000 && Number.isFinite(income)) {
-        tax = Math.round(income * 0.30);
+        tax = Math.round(15000 * 0.13 + (50000 - 15000) * 0.2 + (income - 50000) * 0.30);
         console.log(`Сумма налога составляет ${tax} руб.`);
     } else {
         console.log('Вы ввели  некорректные данные.');
